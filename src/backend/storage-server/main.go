789fc3a3e2671fc357a267c0edec7b9f1886e2fe
kor-Chipmunk <rhj4862@gmail.com>
2024-01-05 11:50:59 +0900

    feat: 실행 환경 변수 적용 / 로깅 추가

--- src/backend/storage-server/main.go
+++ src/backend/storage-server/main.go
@@ -10,15 +10,19 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
-	"time"
 
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
 )
 
 func main() {
-    log.Println("starting Web Server")
 	loadEnvironments()
+	configureLogger()
+
+	logger.Info("starting Web Server")
+
     registerHandlers()
 	listenServe()
 }
@@ -27,13 +31,44 @@ func loadEnvironments() {
 	godotenv.Load(".env")
 }
 
+type ReleaseType int
+const (
+	DEVELOP 	ReleaseType = iota
+	RELEASE
+	PRODUCTION
+)
+
+var releaseLookup = map[string]ReleaseType {
+	"": DEVELOP,
+	"develop": DEVELOP,
+	"release": RELEASE,
+	"production": PRODUCTION,
+}
+
+var logger *zap.Logger
+func configureLogger() {
+	environment := releaseLookup[os.Getenv("ENV")]
+	switch environment {
+	case DEVELOP:
+		logger = zap.Must(zap.NewDevelopment())
+		break
+	case RELEASE:
+		logger = zap.Must(zap.NewDevelopment())
+		break
+	case PRODUCTION:
+		logger = zap.Must(zap.NewProduction())
+	}
+
+	defer logger.Sync()
+}
+
 func listenServe() {
 	port := ":" + os.Getenv("PORT")
     err := http.ListenAndServe(port, nil)
     if err != nil {
         log.Fatalln(err)
     }
-	log.Printf("The server is listening on http://localhost:%s", port)
+	logger.Info("The server is listening on http://localhost:" + port)
 }
 
 func registerHandlers() {
@@ -47,14 +82,14 @@ func handleMusicStaticFiles(w http.ResponseWriter, r *http.Request) {
 		urlEncodedResourceName := strings.TrimPrefix(r.URL.Path, "/")
 		resourceName, err := url.QueryUnescape(urlEncodedResourceName)
 		if err != nil {
-			log.Println(err)
+			logger.Error(err.Error())
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
 
 		audioFile, err := os.ReadFile("./resources/" + resourceName)
 		if err != nil {
-			log.Println(err)
+			logger.Error(err.Error())
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
@@ -62,22 +97,25 @@ func handleMusicStaticFiles(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "audio/mpeg")
 		
-		data := []byte(time.Now().String())
-		etag := fmt.Sprintf("%x", md5.Sum(data))
+		etag := fmt.Sprintf("%x", md5.Sum([]byte(resourceName)))
 		w.Header().Set("Cache-Control", "public, max-age=600")
 		w.Header().Set("ETag", etag)
 
 		if match := w.Header().Get("If-None-Match"); match != "" {
 			if strings.Contains(match, etag) {
 				w.WriteHeader(http.StatusNotModified)
+				logger.Info("access hit file : " + resourceName)
 				break
 			}
 		}
-		
+
 		audioChunks := bytes.NewBuffer(audioFile)
 		if _, err := audioChunks.WriteTo(w); err != nil {
-			log.Println(err)
+			logger.Warn(err.Error())
 		}
+		w.Header().Set("Content-Length", strconv.Itoa(len(audioChunks.Bytes())))
+
+		logger.Info("access miss file : " + resourceName)
 		break
 	}
 }
@@ -85,36 +123,35 @@ func handleMusicStaticFiles(w http.ResponseWriter, r *http.Request) {
 func handleUploadMusic(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		fmt.Println("File Upload Endpoint Hit")
+		logger.Info("File Upload Endpoint Hit")
 
 		// Limit to 10 MB
 		r.ParseMultipartForm(10 << 20)
 
 		file, handler, err := r.FormFile("upload")
 		if err != nil {
-			fmt.Println("Error Retrieving the File")
-			fmt.Println(err)
+			logger.Error(err.Error())
 			return
 		}	
 		defer file.Close()
 
-		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-		fmt.Printf("File Size: %+v\n", handler.Size)
-		fmt.Printf("MIME Header: %+v\n", handler.Header)
+		logger.Sugar().Infof("Uploaded File: %+v", handler.Filename)
+		logger.Sugar().Infof("File Size: %+v", handler.Size)
+		logger.Sugar().Infof("MIME Header: %+v", handler.Header)
 
 		tempFile, err := os.Create(filepath.Join("./resources", handler.Filename))
 		if err != nil {
-			fmt.Println(err)
+			logger.Warn(err.Error())
 		}
 		defer tempFile.Close()
 	
 		fileBytes, err := io.ReadAll(file)
 		if err != nil {
-			fmt.Println(err)
+			logger.Warn(err.Error())
 		}
 		
 		tempFile.Write(fileBytes)
-		fmt.Fprintf(w, "Successfully Uploaded File\n")
+		logger.Info("Successfully Uploaded File")
 		break
 	}
 }
