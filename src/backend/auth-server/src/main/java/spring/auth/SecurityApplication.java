f085b558887576b1d5d7dfe7130bde4086787cea
happysoy <jeongsoyeon0130@naver.com>
2024-01-08 20:56:48 +0900

    refactor: feat#11에서 팔로우 부분 삭제

--- src/backend/auth-server/src/main/java/spring/auth/SecurityApplication.java
+++ src/backend/auth-server/src/main/java/spring/auth/SecurityApplication.java
@@ -1,10 +1,7 @@
 package spring.auth;
 
-import org.neo4j.cypherdsl.core.renderer.Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.neo4j.cypherdsl.core.renderer.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
