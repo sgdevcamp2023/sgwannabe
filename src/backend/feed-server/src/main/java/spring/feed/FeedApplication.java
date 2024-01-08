8d7789d88d6d4fb8c63d4c0096ea581aec27be03
happysoy <jeongsoyeon0130@naver.com>
2024-01-08 23:28:08 +0900

    feat: 피드 서버 구축

---
+++ src/backend/feed-server/src/main/java/spring/feed/FeedApplication.java
@@ -0,0 +1,13 @@
+package spring.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedApplication.class, args);
+	}
+
+}
