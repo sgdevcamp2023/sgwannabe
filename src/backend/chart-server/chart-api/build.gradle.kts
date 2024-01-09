44aa189e4b9351c1f61d227f5340a1ccd55c4e54
kor-Chipmunk <rhj4862@gmail.com>
2024-01-09 09:50:28 +0900

    feat: 몽고디비 연결 설정 추가

--- src/backend/chart-server/chart-api/build.gradle.kts
+++ src/backend/chart-server/chart-api/build.gradle.kts
@@ -3,6 +3,15 @@ tasks.getByName("bootJar") {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("io.projectreactor:reactor-test")
 }
---
+++ src/backend/chart-server/chart-api/src/main/kotlin/com/lalala/chart/config/MongoConfiguration.kt
@@ -0,0 +1,41 @@
+package com.lalala.chart.config
+
+import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClients
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions
+import org.springframework.data.mongodb.core.convert.NoOpDbRefResolver
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+
+@Configuration
+@EnableReactiveMongoRepositories(basePackages = ["com.lalala.chart.repository"])
+@EnableReactiveMongoAuditing
+class MongoConfiguration(
+    private val env: Environment
+) : AbstractReactiveMongoConfiguration() {
+
+    override fun getDatabaseName() = "chart"
+
+    override fun reactiveMongoClient() = mongoClient()
+    
+    @Bean
+    fun mongoClient(): MongoClient {
+        val uri = env.getProperty("spring.data.mongodb.uri") ?: "mongodb://localhost"
+        return MongoClients.create(uri)
+    }
+
+    @Bean
+    override fun reactiveMongoTemplate(
+        databaseFactory: ReactiveMongoDatabaseFactory,
+        mongoConverter: MappingMongoConverter
+    ): ReactiveMongoTemplate = ReactiveMongoTemplate(mongoClient(), databaseName)
+}
