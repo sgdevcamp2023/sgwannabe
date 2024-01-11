481733861a4f3c23460ade15961d661991d7a24a
happysoy <jeongsoyeon0130@naver.com>
2024-01-11 19:26:49 +0900

    feat: 유저 서버를 구축합니다

---
+++ src/backend/user-server/src/test/java/userserver/UserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package userserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
