6a10fc77a1a75457cc1d8a7bc043c608198caed5
kor-Chipmunk <rhj4862@gmail.com>
2024-01-17 01:13:51 +0900

    feat: SpringBoot3 전용 Swagger 의존성 추가 / Swagger 설정

---
+++ src/backend/music-server/music-api/src/main/java/com/lalala/music/config/SwaggerConfiguration.java
@@ -0,0 +1,19 @@
+package com.lalala.music.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("음원 API")
+                        .description("음원 API를 제공합니다.")
+                        .version("0.0.1")
+                );
+    }
+}
