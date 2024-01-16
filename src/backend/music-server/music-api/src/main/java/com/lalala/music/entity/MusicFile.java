57942145e9d5d4d735a6442e8b3d2678fb1deb64
kor-Chipmunk <rhj4862@gmail.com>
2024-01-17 00:53:42 +0900

    feat: 음악 파일 클래스 생성자 및 게터 추가

--- src/backend/music-server/music-api/src/main/java/com/lalala/music/entity/MusicFile.java
+++ src/backend/music-server/music-api/src/main/java/com/lalala/music/entity/MusicFile.java
@@ -4,8 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MusicFile {
     @Column(name = "file_url", nullable = false, columnDefinition = "VARCHAR(255) default ''")
     Long fileUrl;
