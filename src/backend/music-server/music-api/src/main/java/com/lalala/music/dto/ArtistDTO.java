35cc84cb868351b374dee6febe873a77f4d33083
kor-Chipmunk <rhj4862@gmail.com>
2024-01-17 01:00:20 +0900

    feat: 아티스트 DTO 추가

---
+++ src/backend/music-server/music-api/src/main/java/com/lalala/music/dto/ArtistDTO.java
@@ -0,0 +1,33 @@
+package com.lalala.music.dto;
+
+import com.lalala.music.entity.ArtistEntity;
+import com.lalala.music.entity.ArtistType;
+import com.lalala.music.entity.GenderType;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ArtistDTO {
+    private final Long id;
+    private final String name;
+    private final GenderType genderType;
+    private final ArtistType type;
+    private final String agency;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public static ArtistDTO from(ArtistEntity artist) {
+        return new ArtistDTO(
+                artist.getId(),
+                artist.getName(),
+                artist.getGender(),
+                artist.getType(),
+                artist.getAgency(),
+                artist.getCreatedAt(),
+                artist.getUpdatedAt()
+        );
+    }
+}
