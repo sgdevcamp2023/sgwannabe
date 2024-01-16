3cadbf3dbf27cec8d9bd6e1dfce789dd64c662f7
sua kang <rkdtndk09@gmail.com>
2024-01-16 11:05:46 +0900

    feat: STOMP 적용

--- src/backend/chatting-server/src/main/java/chattingserver/handlers/WebSocketChatHandler.java
+++ src/backend/chatting-server/src/main/java/chattingserver/handlers/WebSocketChatHandler.java
@@ -1,2 +1,40 @@
-package chattingserver.handlers;public class WebSocketChatHandler {
+package chattingserver.handlers;
+
+import chattingserver.dto.ChatMessageDto;
+import chattingserver.service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload={}", payload);
+        ChatMessageDto chatMessage = objectMapper.readValue(payload, ChatMessageDto.class);
+        chatService.sendMessage(session, chatMessage);
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("connected websocket session={}", session.toString());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("closed websocket session={}", session.toString());
+        super.afterConnectionClosed(session, status);
+    }
 }
