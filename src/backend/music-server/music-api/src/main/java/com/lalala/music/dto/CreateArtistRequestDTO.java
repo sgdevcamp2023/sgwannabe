d881344b09537a2a0145b7c808f41bc9c258d299
kor-Chipmunk <rhj4862@gmail.com>
2024-01-17 01:04:04 +0900

    feat: 아티스트 생성 / 수정 요청 DTO 추가

---
+++ src/backend/music-server/music-api/src/main/java/com/lalala/music/dto/CreateArtistRequestDTO.java
@@ -0,0 +1,18 @@
+package com.lalala.music.dto;
+
+import com.lalala.music.entity.ArtistType;
+import com.lalala.music.entity.GenderType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class CreateArtistRequestDTO {
+    private String name;
+    private GenderType gender;
+    private ArtistType type;
+    private String agency;
+}
