c228c7d6d435697764428b9a171c84281f27b9a7
kor-Chipmunk <rhj4862@gmail.com>
2024-01-17 15:05:11 +0900

    feat: `scheme.sql` 칼럼 변경 적용

--- src/backend/music-server/infra/scheme.sql
+++ src/backend/music-server/infra/scheme.sql
@@ -8,13 +8,13 @@ USE music;
 -- 테이블 생성 SQL - artists
 CREATE TABLE artists
 (
-    `id`          BIGINT      NOT NULL    AUTO_INCREMENT COMMENT '고유 번호',
-    `name`        CHAR(50)    NOT NULL    DEFAULT '' COMMENT '이름',
-    `gender`      CHAR(6)     NOT NULL    DEFAULT 'MALE' COMMENT '성별. MALE / FEMALE',
-    `type`        CHAR(5)     NOT NULL    DEFAULT 'SOLO' COMMENT '아티스트 구분. SOLO / GROUP',
-    `agency`      CHAR(50)    NOT NULL    COMMENT '소속사',
-    `created_at`  DATETIME    NOT NULL    DEFAULT NOW() COMMENT '생성 시각',
-    `updated_at`  DATETIME    NOT NULL    DEFAULT NOW() COMMENT '수정 시각',
+    `id`          BIGINT        NOT NULL    AUTO_INCREMENT COMMENT '고유 번호',
+    `name`        VARCHAR(50)   NOT NULL    DEFAULT '' COMMENT '이름',
+    `gender`      VARCHAR(6)    NOT NULL    DEFAULT 'MALE' COMMENT '성별. MALE / FEMALE / MIX',
+    `type`        VARCHAR(5)    NOT NULL    DEFAULT 'SOLO' COMMENT '아티스트 구분. SOLO / GROUP',
+    `agency`      VARCHAR(50)   NOT NULL    COMMENT '소속사',
+    `created_at`  DATETIME      NOT NULL    DEFAULT NOW() COMMENT '생성 시각',
+    `updated_at`  DATETIME      NOT NULL    DEFAULT NOW() COMMENT '수정 시각',
      PRIMARY KEY (id)
 );
 
@@ -28,10 +28,11 @@ CREATE TABLE albums
 (
     `id`           BIGINT          NOT NULL    AUTO_INCREMENT COMMENT '고유 번호', 
     `artist_id`    BIGINT          NOT NULL    COMMENT '아티스트', 
-    `type`         CHAR(7)         NOT NULL    DEFAULT 'SINGLE' COMMENT '앨범 구분. SINGLE / REGULAR', 
-    `title`        VARCHAR(50)     NOT NULL    DEFAULT '' COMMENT '제목', 
-    `released_at`  DATETIME        NOT NULL    COMMENT '발매일', 
-    `like_count`   INT UNSIGNED    NOT NULL    DEFAULT 0 COMMENT '좋아요', 
+    `type`         VARCHAR(7)      NOT NULL    DEFAULT 'SINGLE' COMMENT '앨범 구분. SINGLE / REGULAR',
+    `title`        VARCHAR(50)     NOT NULL    DEFAULT '' COMMENT '제목',
+    `released_at`  DATETIME        NOT NULL    COMMENT '발매일',
+    `cover_url`    VARCHAR(255)    NOT NULL    COMMENT '앨범 커버 주소',
+    `like_count`   INT UNSIGNED    NOT NULL    DEFAULT 0 COMMENT '좋아요',
     `created_at`   DATETIME        NOT NULL    DEFAULT NOW() COMMENT '생성 시각', 
     `updated_at`   DATETIME        NOT NULL    DEFAULT NOW() COMMENT '수정 시각', 
      PRIMARY KEY (id)
@@ -61,7 +62,7 @@ CREATE TABLE musics
     `play_time`    SMALLINT UNSIGNED    NOT NULL    DEFAULT 0 COMMENT '재생 시간',
     `lyrics`       TEXT                 NOT NULL    COMMENT '가사',
     `file_url`     VARCHAR(255)         NOT NULL    DEFAULT '' COMMENT '파일 주소',
-    `format_type`  CHAR(5)              NOT NULL    DEFAULT 'MP3' COMMENT '파일 형식. MP3 / FLAC',
+    `format_type`  VARCHAR(5)           NOT NULL    DEFAULT 'MP3' COMMENT '파일 형식. MP3 / FLAC',
     `like_count`   INT UNSIGNED         NOT NULL    DEFAULT 0 COMMENT '좋아요',
     `created_at`   DATETIME             NOT NULL    DEFAULT NOW() COMMENT '생성 시각',
     `updated_at`   DATETIME             NOT NULL    DEFAULT NOW() COMMENT '수정 시각',
