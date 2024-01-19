d32d232c141a3bad71b4218b98c516a5cc4c6037
sua kang <rkdtndk09@gmail.com>
2024-01-19 16:17:12 +0900

    feat: 데이터 색인  api 구현

--- src/backend/search-server/src/api/indexing_api.py
+++ src/backend/search-server/src/api/indexing_api.py
@@ -1,10 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from ..elasticsearch.elasticsearch_connector import ElasticsearchConnector
+from pydantic import BaseModel
+
+from ..models.music import Music
+from ..models.album import Album
+from ..models.artist import Artist
 
 router = APIRouter(tags=["indexing API"])
 es_connector = ElasticsearchConnector(host='elasticsearch', port=9200)
 
-@router.post("/index")
-def index_data(index: str, data: dict):
-    es_connector.index_data(index, [data])
-    return {"message": "es 인덱싱 성공"}
+class IndexRequest(BaseModel):
+    data: dict
+
+@router.post("/index/{index_type}")
+def index_data(index_type:str, request_body:IndexRequest):
+    """
+    데이터를 Elasticsearch에 색인합니다.
+    """
+    data = request_body.data
+
+    try:
+        es_connector.index(index=f"{index_type}_index", body=data)
+        return {"message": f"{index_type} 색인 성공"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{index_type} 색인 실패: {str(e)}")
