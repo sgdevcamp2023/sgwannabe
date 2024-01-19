79b073aefd4f28af23adce916e746be62ffbfccc
kor-Chipmunk <rhj4862@gmail.com>
2024-01-19 17:35:22 +0900

    feat: 파일서버 오디오 파일 연동하기

--- src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/controller/StreamingController.kt
+++ src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/controller/StreamingController.kt
@@ -1,5 +1,7 @@
 package com.lalala.streaming.controller
 
+import lombok.RequiredArgsConstructor
+import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.UrlResource
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,18 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/streaming")
 class StreamingController(
-    val audioLocation: String = "static"
+    val audioLocation: String = "http://localhost:20000"
 ) {
     @GetMapping
-    fun getAudio(@RequestParam("name") name: String): ResponseEntity<UrlResource> {
-        val audio = UrlResource("file:$audioLocation/$name")
+    fun getAudio(@RequestParam("name") name: String): ResponseEntity<ByteArrayResource> {
+        val audio = UrlResource("$audioLocation/$name")
         return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
             .contentType(MediaTypeFactory
                 .getMediaType(audio)
                 .orElse(MediaType.APPLICATION_OCTET_STREAM))
-            .body(audio)
+            .body(ByteArrayResource(audio.inputStream.readAllBytes()))
     }
 }
