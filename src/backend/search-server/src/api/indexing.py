2fdcbc2a446ce6702b79cc352a7a9f1501d294de
sua kang <rkdtndk09@gmail.com>
2024-01-22 15:25:39 +0900

    refactor: indexing api 도메인 분리

--- src/backend/search-server/src/api/indexing.py
+++ src/backend/search-server/src/api/indexing.py
@@ -40,30 +40,44 @@ index_type_models = {
     'music': MusicIndexRequest,
 }
 
-def get_model(index_type: str) -> Type[BaseModel]:
-    model = index_type_models.get(index_type.lower())
-    if model is None:
-        raise HTTPException(status_code=400, detail=f"잘못된 인덱스 유형: {index_type}")
-    return model
-
-@router.post("/index/{index_type}")
-async def index_data(index_type: str, body: MusicIndexRequest = Body(embed=True)):
+@router.post("/index/music")
+async def index_data(body: MusicIndexRequest = Body(embed=True)):
     """
-    데이터를 Elasticsearch에 색인합니다.
+    Music 데이터를 Elasticsearch에 색인합니다.
     """
-    # print(body.body.model_dump())
     print(body)
 
     try:
-        # 인덱스 유형에 해당하는 Pydantic 모델 가져오기
-        # model = get_model(index_type)
-
-        # 데이터를 지정된 모델로 유효성 검사 및 변환
-        # index_data = model.model_construct(**body.dict())
-
-        # Elasticsearch 색인 수행
-        es_connector.index(index=f"{index_type}_index", data=body.dict())
+        es_connector.index(index=f"music_index", data=body.model_dump())
         
-        return {"message": f"{index_type} 색인 성공"}
+        return {"message": f"music 색인 성공"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"{index_type} 색인 실패: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"music 색인 실패: {str(e)}")
+    
+@router.post("/index/artist")
+async def index_data(body: ArtistIndexRequest = Body(embed=True)):
+    """
+    Artist 데이터를 Elasticsearch에 색인합니다.
+    """
+    print(body)
+
+    try:
+        es_connector.index(index=f"artist_index", data=body.model_dump())
+        
+        return {"message": f"artist 색인 성공"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"artist 색인 실패: {str(e)}")
+    
+@router.post("/index/album")
+async def index_data(body: AlbumIndexRequest = Body(embed=True)):
+    """
+    Album 데이터를 Elasticsearch에 색인합니다.
+    """
+    print(body)
+
+    try:
+        es_connector.index(index=f"album_index", data=body.model_dump())
+        
+        return {"message": f"album 색인 성공"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"album 색인 실패: {str(e)}")
--- src/backend/search-server/src/elasticsearch/elasticsearch_connector.py
+++ src/backend/search-server/src/elasticsearch/elasticsearch_connector.py
@@ -6,7 +6,6 @@ class ElasticsearchConnector:
         self.es = Elasticsearch([{'host': host, 'port': port, 'scheme': 'http'}])
 
     def index(self, index, data):
-        # bulk(self.es, data, index=index)
         self.es.index(index=index, body=data)
 
     def search(self, index, query):
