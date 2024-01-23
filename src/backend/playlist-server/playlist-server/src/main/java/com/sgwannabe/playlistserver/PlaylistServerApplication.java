fbf12c63df0c8485753d874aeafb7595f9044fca
sua kang <rkdtndk09@gmail.com>
2024-01-23 11:41:31 +0900

    feat: 프로젝트 세팅 & 도커 환경 설정

---
+++ src/backend/playlist-server/playlist-server/src/main/java/com/sgwannabe/playlistserver/PlaylistServerApplication.java
@@ -0,0 +1,13 @@
+package com.sgwannabe.playlistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistServerApplication.class, args);
+	}
+
+}
