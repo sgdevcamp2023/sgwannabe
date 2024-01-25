1a58b160f2dda567203d4c07bad26190287b1df9
sua kang <rkdtndk09@gmail.com>
2024-01-25 18:54:02 +0900

    chore: swagger 설정

---
+++ src/backend/playlist-server/playlist-server/src/main/java/com/sgwannabe/playlistserver/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.sgwannabe.playlistserver.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Playlist-Server",
+                description = "playlist server api명세",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("PLAYLIST-SERVER API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
