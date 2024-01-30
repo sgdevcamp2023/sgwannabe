964f1094f9284cc92c7450b1ced9eb440048955d
happysoy <jeongsoyeon0130@naver.com>
2024-01-30 16:59:26 +0900

    feat: 초기 세팅

---
+++ src/backend/presence-server/src/main/java/presenceserver/PresenceServerApplication.java
@@ -0,0 +1,13 @@
+package presenceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceServerApplication.class, args);
+	}
+
+}
---
+++ src/backend/presence-server/src/test/java/presenceserver/PresenceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package presenceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
