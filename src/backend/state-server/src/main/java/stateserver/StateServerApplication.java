69af709006f297673e864c67268a4aa2b734848d
happysoy <jeongsoyeon0130@naver.com>
2024-01-30 15:13:27 +0900

    feat: docker compose 파일 추가

---
+++ src/backend/state-server/src/main/java/stateserver/StateServerApplication.java
@@ -0,0 +1,13 @@
+package stateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateServerApplication.class, args);
+	}
+
+}
---
+++ src/backend/state-server/src/test/java/stateserver/StateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package stateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
