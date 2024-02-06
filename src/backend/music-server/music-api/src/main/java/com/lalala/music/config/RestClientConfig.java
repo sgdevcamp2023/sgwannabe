358c76c71b5406d0b15cb8dbb641b45c2e5352c1
kor-Chipmunk <rhj4862@gmail.com>
2024-02-06 18:25:57 +0900

    feat: `RestClient` 설정 클래스 `RestClientConfig` 작성

---
+++ src/backend/music-server/music-api/src/main/java/com/lalala/music/config/RestClientConfig.java
@@ -0,0 +1,19 @@
+package com.lalala.music.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+    @Value("${uploader.url:http://localhost:25000}")
+    private String uploaderURL;
+
+    @Bean(name = {"uploaderClient"})
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl(uploaderURL)
+                .build();
+    }
+}
