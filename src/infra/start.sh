1575ca7ca0b73eb88dfeddf597c5b40aaa30aad6
kor-Chipmunk <rhj4862@gmail.com>
2024-02-07 10:35:14 +0900

    infra: 도커 네트워크 추가

--- src/infra/start.sh
+++ src/infra/start.sh
@@ -2,6 +2,17 @@
 
 docker network create --driver bridge lalala-network
 
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+# Infra Setup
+
+cd "$script_dir"
+
+docker-compose down
+docker-compose up -d --build
+
+# Backend Setup
+
 cd "../backend/"
 
 BACKEND_PWD=`pwd`
