1a87a311449d466fe8e5ffb8ea399f44eab1dba3
kor-Chipmunk <rhj4862@gmail.com>
2024-02-07 10:58:16 +0900

    style: spotlessApply 적용

--- src/backend/music-server/music-api/src/main/java/com/lalala/music/util/ArtistUtils.java
+++ src/backend/music-server/music-api/src/main/java/com/lalala/music/util/ArtistUtils.java
@@ -1,16 +1,18 @@
 package com.lalala.music.util;
 
-import com.lalala.music.exception.BusinessException;
-import com.lalala.music.exception.ErrorCode;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import com.lalala.music.entity.ArtistEntity;
+import com.lalala.music.exception.BusinessException;
+import com.lalala.music.exception.ErrorCode;
 import com.lalala.music.repository.ArtistRepository;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ArtistUtils {
     public static ArtistEntity findById(Long id, ArtistRepository repository) {
-        return repository.findById(id).orElseThrow(() -> new BusinessException(ErrorCode.ARTIST_NOT_FOUND));
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ARTIST_NOT_FOUND));
     }
 }
