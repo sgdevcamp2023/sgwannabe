97d003513393502c5f0d8fce3c5b290ad62b6686
sua kang <rkdtndk09@gmail.com>
2024-02-13 18:11:22 +0900

    bugfix: CORS

--- src/backend/chatting-server/src/main/java/chattingserver/config/WebConfig.java
+++ src/backend/chatting-server/src/main/java/chattingserver/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
     }
 
 }
