6ef2d58e3fff738394da6fc92b3b13122308cc39
mingzooo <joozzoo0223@gmail.com>
2024-02-14 21:52:30 +0900

    fix: router적용 및 useParams type에러

--- src/frontend/sg_lalala/src/components/Chat/MessageComponent.tsx
+++ src/frontend/sg_lalala/src/components/Chat/MessageComponent.tsx
@@ -6,7 +6,7 @@ function MessageComponent({ content, nickName, senderProfileImage }: Message) {
     <div className="flex flex-row items-center w-full my-2 text-md font-500">
       <img
         src={senderProfileImage === "" ? person_icon : senderProfileImage}
-        className="object-contain w-8 mr-3 bg-white border-2 rounded-full border-primary"
+        className="object-cover w-8 h-8 mr-3 bg-white border-2 rounded-full border-primary"
       />
       <div className="mr-3 text-primary">{nickName}</div>
       <div className="text-white">{content}</div>
