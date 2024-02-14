d630aefd4c20e11f9e3833f13284329f55eed850
happysoy <jeongsoyeon0130@naver.com>
2024-02-14 15:43:21 +0900

    fix: 충돌 해결 및 init.sql 추가

---
+++ src/backend/user-server/infra/init.sql
@@ -0,0 +1,7 @@
+extracted("신짱구", "jjangu@gmail.com", "aaaaaaaaaa", Status.ACTIVE);
+extracted("신짱아", "jjanga@gmail.com", "aaaaaaaaaa", Status.BLOCK);
+extracted("봉미선", "misun@gmail.com", "aaaaaaaaaa", Status.DELETE);
+extracted("신형만", "man@gmail.com", "aaaaaaaaaa", Status.ACTIVE);
+extracted("흰둥이", "doong@gmail.com", "aaaaaaaaaa", Status.ACTIVE);
+
+INSERT INTO 바꾸기
