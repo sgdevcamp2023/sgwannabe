a0987431ef5bf7ed4175714b06e953b612c6f88e
kor-Chipmunk <rhj4862@gmail.com>
2024-02-14 13:31:49 +0900

    chore: `EnableEurekServer` application 에서 삭제

--- src/backend/discovery-server/src/main/java/com/lalala/discovery/DiscoveryApplication.java
+++ src/backend/discovery-server/src/main/java/com/lalala/discovery/DiscoveryApplication.java
@@ -2,10 +2,8 @@ package com.lalala.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
