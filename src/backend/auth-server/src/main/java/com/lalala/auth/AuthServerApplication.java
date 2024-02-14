d02770110811c9063c524c315d926d1f2efc089f
kor-Chipmunk <rhj4862@gmail.com>
2024-02-14 17:39:14 +0900

    feat: auth `BaseResponseBodyAdvice` 추가

--- src/backend/auth-server/src/main/java/com/lalala/auth/AuthServerApplication.java
+++ src/backend/auth-server/src/main/java/com/lalala/auth/AuthServerApplication.java
@@ -3,6 +3,7 @@ package com.lalala.auth;
 import com.lalala.aop.AuthenticationContext;
 import com.lalala.aop.PassportAspect;
 import com.lalala.config.CommonModuleConfig;
+import com.lalala.response.BaseResponseBodyAdvice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Import;
         CommonModuleConfig.class,
         AuthenticationContext.class,
         PassportAspect.class,
+        BaseResponseBodyAdvice.class
 })
 @SpringBootApplication
 public class AuthServerApplication {
