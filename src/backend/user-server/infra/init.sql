426a7ad1f450b449f037f5386213488cabc47fd2
kor-Chipmunk <rhj4862@gmail.com>
2024-02-14 18:09:08 +0900

    infra: user `created_at`, `updated_at` 기본 값 추가 & 초기 데이터 추가

---
+++ src/backend/user-server/infra/init.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` (email, nickname, password, updated_at) VALUE ("jjangu@gmail.com", "신짱구", "jjangu123");
+
+INSERT INTO `user` (email, nickname, password, updated_at) VALUE ("jjanga@gmail.com", "신짱아", "jjanga123");
+
+INSERT INTO `user` (email, nickname, password, updated_at) VALUE ("doonge@gmail.com", "흰둥이", "doonge123");
