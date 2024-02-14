7cd1b9bc28722e22237f9985bf0a220c0917005a
kor-Chipmunk <rhj4862@gmail.com>
2024-02-15 06:20:26 +0900

    fix: `passport.algorithm` 명 수정

--- src/backend/common-module/src/main/java/com/lalala/exception/ErrorResponse.java
+++ src/backend/common-module/src/main/java/com/lalala/exception/ErrorResponse.java
@@ -8,7 +8,7 @@ import com.lalala.response.BaseResponse;
 public class ErrorResponse extends BaseResponse<Object> {
 
     private ErrorResponse(final ErrorCode code) {
-        super(code.getStatus(), code.getMessage(), code.getCode(), null);
+        super(code.getStatus(), code.getCode(), code.getMessage(), null);
     }
 
     public static ErrorResponse of(final ErrorCode code) {
