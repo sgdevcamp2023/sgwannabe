e61d2a67130a2987f598e39bbe28f27cb830d5d6
kor-Chipmunk <rhj4862@gmail.com>
2024-02-15 06:21:16 +0900

    feat: 게이트웨이 예외 핸들러 추가

--- src/backend/gateway-server/src/main/java/com/lalala/gateway/GatewayApplication.java
+++ src/backend/gateway-server/src/main/java/com/lalala/gateway/GatewayApplication.java
@@ -2,7 +2,11 @@ package com.lalala.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.lalala.exception.GlobalExceptionHandler;
+
+@Import({GlobalExceptionHandler.class})
 @SpringBootApplication
 public class GatewayApplication {
 
