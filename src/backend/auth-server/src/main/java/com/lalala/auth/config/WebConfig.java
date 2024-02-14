a0243262d423237e320b3e2644289185b0df3e7e
kor-Chipmunk <rhj4862@gmail.com>
2024-02-14 17:39:36 +0900

    feat: `auth` WebConfig `@Configuration` 추가

--- src/backend/auth-server/src/main/java/com/lalala/auth/config/WebConfig.java
+++ src/backend/auth-server/src/main/java/com/lalala/auth/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.lalala.auth.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
