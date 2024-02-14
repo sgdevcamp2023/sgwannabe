c8e14d89408a2fb77e3d942834340d78df6f09e8
mingzooo <joozzoo0223@gmail.com>
2024-02-14 17:48:19 +0900

    feat: 채팅 화면 및 웹소켓 연동

--- src/frontend/sg_lalala/src/App.tsx
+++ src/frontend/sg_lalala/src/App.tsx
@@ -3,14 +3,16 @@ import { router } from "./routes";
 import { useRecoilValue } from "recoil";
 import { playState } from "./state";
 import MusicBar from "./components/shared/MusicBar";
+import AddSongModal from "./components/shared/AddSongModal";
 
 function App() {
   const nowPlaying = useRecoilValue(playState);
 
   return (
     <div className="flex justify-center h-screen">
-      <div className="flex flex-col w-[1200px] items-center relative">
+      <div className="flex flex-col w-[1200px] items-center relative overflow-y-scroll">
         <RouterProvider router={router} />
+        {/* {true && <AddSongModal />} */}
         {nowPlaying.backgroundPlaying && <MusicBar />}
       </div>
     </div>
