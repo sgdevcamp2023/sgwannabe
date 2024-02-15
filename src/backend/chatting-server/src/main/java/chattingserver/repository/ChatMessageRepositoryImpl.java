7e665c05ddfc71aa79228f4aff0f8626755a49cb
sua kang <rkdtndk09@gmail.com>
2024-02-15 13:23:00 +0900

    style: TODO 제거

--- src/backend/chatting-server/src/main/java/chattingserver/repository/ChatMessageRepositoryImpl.java
+++ src/backend/chatting-server/src/main/java/chattingserver/repository/ChatMessageRepositoryImpl.java
@@ -80,8 +80,7 @@ public class ChatMessageRepositoryImpl implements ChatMessageRepositoryCustom{
         ObjectId readMsgObjectId = new ObjectId(readMsgId);
         Query query = new Query();
         query.addCriteria(Criteria.where("roomId").is(roomId).and("_id").lt(readMsgObjectId));
-        query.limit(20); // 최대 20개의 메시지 가져오도록 제한 설정
+        query.limit(20);
         return mongoTemplate.find(query, ChatMessage.class);
     }
-    //TODO error처리 이전 메시지 못가져오고 빈배열 반환되는중 lt가 문제임
 }
