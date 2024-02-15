a210eac6ba3443c0500104cddf1d8ccb7d3041c4
kor-Chipmunk <rhj4862@gmail.com>
2024-02-15 10:34:57 +0900

    feat: eureka client 코드 추가

--- src/backend/search-server/backend/src/main.py
+++ src/backend/search-server/backend/src/main.py
@@ -1,5 +1,17 @@
+import py_eureka_client.eureka_client as eureka_client
 from fastapi import FastAPI
-from .api import client, indexing, query;
+from .api import client, indexing, query
+
+# eureka_client 에서 nested async await 지원 위한 라이브러리
+import nest_asyncio
+nest_asyncio.apply()
+
+# TODO: host env 으로 받기
+eureka_client.init(
+    eureka_server="http://localhost:34100",
+    app_name="SEARCH-SERVER",
+    instance_port=20000
+)
 
 app = FastAPI()
 
