5a8e367dde14b41c2139264f513773864eb9108f
kor-Chipmunk <rhj4862@gmail.com>
2024-02-15 09:14:59 +0900

    infra: develop 내용 업데이트 해주는 스크립트 작성

---
+++ scripts/update_from_develop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# develop 작업 내용을 모든 서버 develop 에 rebase 으로 합칩니다.
+
+git checkout develop
+git pull
+
+SERVER_ORDER=(
+    "discovery/develop"
+
+    "user/develop"
+
+    "auth/develop"
+
+    "storage/develop"
+    
+    "music/develop"
+    "music-uploader/develop"
+    
+    "streaming/develop"
+    
+    "alarm/develop"
+
+    "chart/develop"
+    
+    "chatting/develop"
+    
+    "feed/develop"
+
+    "playlist/develop"
+
+    "search/develop"
+
+    "gateway/develop"
+)
+
+for SERVER in ${SERVER_ORDER[@]}; do
+    git checkout $SERVER
+    git rebase develop
+    git push -f
+done
+
+git checkout develop
