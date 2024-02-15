cd9e6a751a25d5d5d3db4b6f27ceea4a57eed3ca
kor-Chipmunk <rhj4862@gmail.com>
2024-02-15 16:23:26 +0900

    fix: music 에서만 작동되는 응답 핸들러 제거

--- src/backend/music-server/music-api/src/main/java/com/lalala/music/exception/UploadFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.lalala.music.exception;
-
-public class UploadFailedException extends BusinessException {
-    public UploadFailedException() {
-        super(ErrorCode.MUSIC_UPLOAD_FAILED);
-    }
-}
