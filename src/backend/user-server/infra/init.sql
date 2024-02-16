73277b2ee33f0d02b0f0526ef41557c203fcbd5b
kor-Chipmunk <rhj4862@gmail.com>
2024-02-16 11:30:49 +0900

    fix: init.sql 삭제

--- src/backend/user-server/infra/init.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user` (email, nickname, password, updated_at) VALUE ("jjangu@gmail.com", "신짱구", "jjangu123");
-
-INSERT INTO `user` (email, nickname, password, updated_at) VALUE ("jjanga@gmail.com", "신짱아", "jjanga123");
-
-INSERT INTO `user` (email, nickname, password, updated_at) VALUE ("doonge@gmail.com", "흰둥이", "doonge123");
