f45aa5ef2900ef5bac20eb7112baa167c1f806a3
kor-Chipmunk <rhj4862@gmail.com>
2024-02-16 15:33:57 +0900

    infra: restore

---
+++ src/backend/chatting-server/src/main/java/chattingserver/ChattingApplication.java
@@ -0,0 +1,22 @@
+package chattingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.lalala.aop.AuthenticationContext;
+import com.lalala.aop.PassportAspect;
+import com.lalala.config.CommonModuleConfig;
+
+@Import({
+		CommonModuleConfig.class,
+		AuthenticationContext.class,
+		PassportAspect.class,
+})
+@SpringBootApplication
+public class ChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingApplication.class, args);
+	}
+}
