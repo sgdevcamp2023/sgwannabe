d0986969fc56a09461f27da9b0c4d82c713ab19f
kor-Chipmunk <rhj4862@gmail.com>
2024-02-18 01:24:39 +0900

    chore: chart 서버 Reactive 공통 모듈 적용

--- src/backend/chart-server/chart-api/src/main/kotlin/com/lalala/chart/ChartApplication.kt
+++ src/backend/chart-server/chart-api/src/main/kotlin/com/lalala/chart/ChartApplication.kt
@@ -1,16 +1,16 @@
 package com.lalala.chart
 
 import com.lalala.aop.AuthenticationContext
-import com.lalala.aop.PassportAspect
-import com.lalala.config.CommonModuleConfig
 import com.lalala.config.KafkaConsumerConfig
+import com.lalala.reactive.aop.PassportAspect
+import com.lalala.reactive.config.CommonReactiveModuleConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
 @SpringBootApplication
 @Import(
-    CommonModuleConfig::class,
+    CommonReactiveModuleConfig::class,
     AuthenticationContext::class,
     PassportAspect::class,
     KafkaConsumerConfig::class,
--- src/backend/chart-server/settings.gradle.kts
+++ src/backend/chart-server/settings.gradle.kts
@@ -1,6 +1,16 @@
 rootProject.name = "chart"
 
+include ("chart-api")
+
+
 include(":common-module")
 findProject(":common-module")?.projectDir = file("../common-module")
 
-include ("chart-api")
+include(":common-module:common")
+findProject(":common-module:common")?.projectDir = file("../common-module/common")
+
+include(":common-module:common-mvc")
+findProject(":common-module:common-mvc")?.projectDir = file("../common-module/common-mvc")
+
+include(":common-module:common-reactive")
+findProject(":common-module:common-reactive")?.projectDir = file("../common-module/common-reactive")
