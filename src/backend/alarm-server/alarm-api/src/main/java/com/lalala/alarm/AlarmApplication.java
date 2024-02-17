abad391d2fdf16cb25f7b8617dcd966c71ed5255
kor-Chipmunk <rhj4862@gmail.com>
2024-02-18 01:24:01 +0900

    chore: alarm 서버 MVC 공통 모듈 적용

--- src/backend/alarm-server/alarm-api/src/main/java/com/lalala/alarm/AlarmApplication.java
+++ src/backend/alarm-server/alarm-api/src/main/java/com/lalala/alarm/AlarmApplication.java
@@ -1,8 +1,25 @@
 package com.lalala.alarm;
 
+import com.lalala.aop.AuthenticationContext;
+import com.lalala.config.KafkaConsumerConfig;
+import com.lalala.config.KafkaProducerConfig;
+import com.lalala.mvc.aop.PassportAspect;
+import com.lalala.mvc.config.CommonMvcModuleConfig;
+import com.lalala.mvc.exception.GlobalExceptionHandler;
+import com.lalala.mvc.response.BaseResponseBodyAdvice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({
+		CommonMvcModuleConfig.class,
+		AuthenticationContext.class,
+		PassportAspect.class,
+		KafkaConsumerConfig.class,
+		KafkaProducerConfig.class,
+		BaseResponseBodyAdvice.class,
+		GlobalExceptionHandler.class,
+})
 @SpringBootApplication
 public class AlarmApplication {
 
