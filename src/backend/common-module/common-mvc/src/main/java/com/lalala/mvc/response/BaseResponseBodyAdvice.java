381451df57837130a834b35f3119c137f2e58614
kor-Chipmunk <rhj4862@gmail.com>
2024-02-19 15:36:17 +0900

    fix: ResponseAdvice Swagger 와 충돌나서 삭제

--- src/backend/common-module/common-mvc/src/main/java/com/lalala/mvc/response/BaseResponseBodyAdvice.java
+++ src/backend/common-module/common-mvc/src/main/java/com/lalala/mvc/response/BaseResponseBodyAdvice.java
@@ -3,15 +3,12 @@ package com.lalala.mvc.response;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
-import com.lalala.response.BaseResponse;
 
 @ControllerAdvice
 @Slf4j
@@ -30,10 +27,6 @@ public class BaseResponseBodyAdvice implements ResponseBodyAdvice<Object> {
             Class<? extends HttpMessageConverter<?>> selectedConverterType,
             ServerHttpRequest request,
             ServerHttpResponse response) {
-        if (body instanceof BaseResponse<?>) {
-            return body;
-        }
-
-        return BaseResponse.from(HttpStatus.OK.value(), "성공했습니다.", body);
+        return body;
     }
 }
