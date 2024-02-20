e6ed137f09c5701ed948ffd6cf736599ae3be41d
kor-Chipmunk <rhj4862@gmail.com>
2024-02-21 07:22:56 +0900

    infra: `music_likes` 테이블 스키마 추가

--- src/backend/music-server/infra/00_schema.sql
+++ src/backend/music-server/infra/00_schema.sql
@@ -91,3 +91,27 @@ ALTER TABLE musics
 -- Foreign Key 삭제 SQL - musics(album_id)
 -- ALTER TABLE musics
 -- DROP FOREIGN KEY FK_musics_album_id_albums_id;
+
+-- musics Table Create SQL
+-- 테이블 생성 SQL - musics
+CREATE TABLE music_likes
+(
+    `id`           BIGINT               NOT NULL    AUTO_INCREMENT COMMENT '고유 번호',
+    `music_id`     BIGINT               NOT NULL    COMMENT '음원',
+    `user_id`      BIGINT               NOT NULL    COMMENT '유저',
+    `created_at`   DATETIME             NOT NULL    DEFAULT NOW() COMMENT '생성 시각',
+    `updated_at`   DATETIME             NOT NULL    DEFAULT NOW() COMMENT '수정 시각',
+     PRIMARY KEY (id)
+);
+
+-- 테이블 Comment 설정 SQL - musics
+ALTER TABLE music_likes COMMENT '음원 좋아요 테이블';
+
+-- Foreign Key 설정 SQL - musics(album_id) -> albums(id)
+ALTER TABLE music_likes
+    ADD CONSTRAINT FK_music_likes_music_id_music_id FOREIGN KEY (music_id)
+        REFERENCES musics (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- Foreign Key 삭제 SQL - music_likes(music_id)
+-- ALTER TABLE music_likes
+-- DROP FOREIGN KEY FK_music_likes_music_id_music_id;
