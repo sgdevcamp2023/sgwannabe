d2f5c253536250fc28f110833380f2cb8f8a5edf
kor-Chipmunk <rhj4862@gmail.com>
2024-02-22 13:19:13 +0900

    feat: user id 4번 ~ 7번 더미데이터 생성

---
+++ src/backend/user-server/infra/02_dummy.sql
@@ -0,0 +1,9 @@
+USE lalala;
+
+SET NAMES utf8mb4;
+
+INSERT INTO `user` (`email`, `last_access`, `nickname`, `password`, `profile`, `role`, `status`, `created_at`, `updated_at`) VALUES
+('seonjae@gmail.com',	NULL,	'선재',	'{bcrypt}$2a$10$LC9vnGOAMa/VxXV2Iu56Pu2mqqSO47LV2bni3LCQaEaLE/8i7DvZ.',	'https://cdn.discordapp.com/avatars/747483070713692251/c6a359f6b0284edf23d4d6de6a4435f7.webp?size=32',	'USER',	'ACTIVE',	'2024-02-22 09:26:59',	'2024-02-22 09:26:59'),
+('sua@gmail.com',	NULL,	'수아',	'{bcrypt}$2a$10$KhH.zNPhKvzffekFjIKEIO2s7F8G/Tx68eKHTaqNPCZBvv8OzL96C',	'https://cdn.discordapp.com/avatars/554274257308483584/75a91b800109785e1e1761f1ced07032.webp?size=32',	'USER',	'ACTIVE',	'2024-02-22 09:29:23',	'2024-02-22 09:29:23'),
+('minzoo@gmail.com',	NULL,	'민주',	'{bcrypt}$2a$10$XSDRqYX0ljunHwvt8KWLW.buJZgXPJeXLc8MEb9H6jRVhToSzFklm',	'',	'USER',	'ACTIVE',	'2024-02-22 09:29:34',	'2024-02-22 09:29:34'),
+('soyeon@gmail.com',	NULL,	'소연',	'{bcrypt}$2a$10$FjdozPpbFhQbH.cq39Pyde.KdUb0GpdPW/bxhqr9dEQXNUxIeWSpe',	'https://cdn.discordapp.com/avatars/990139242468478976/aeffadd53b7e25f5ebee29091377b4ee.webp?size=32',	'USER',	'ACTIVE',	'2024-02-22 09:29:40',	'2024-02-22 09:29:40');
