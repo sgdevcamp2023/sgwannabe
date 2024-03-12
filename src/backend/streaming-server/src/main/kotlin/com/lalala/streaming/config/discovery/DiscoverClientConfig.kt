f4528fbe55994e6f8f85ebb558511ace5144c66b
kor-Chipmunk <rhj4862@gmail.com>
2024-03-12 11:30:51 +0900

    feat: 서비스 디스커버리를 위해 Eureka 서버에 등록하는 Config 추가

---
+++ src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/config/discovery/DiscoverClientConfig.kt
@@ -0,0 +1,8 @@
+package com.lalala.streaming.config.discovery
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableDiscoveryClient
+class DiscoveryClientConfig
