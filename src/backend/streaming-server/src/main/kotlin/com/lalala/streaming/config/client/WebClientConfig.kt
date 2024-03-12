fe373ce161e50e50bc586b540f11451309a6aef7
kor-Chipmunk <rhj4862@gmail.com>
2024-03-12 11:32:32 +0900

    refactor: `RestClient` 대신 Reactive 구조인 `WebClient` 빈 으로 대체

---
+++ src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/config/client/WebClientConfig.kt
@@ -0,0 +1,30 @@
+package com.lalala.streaming.config.client
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.ExchangeStrategies
+import org.springframework.web.reactive.function.client.WebClient
+
+const val SIZE_MB = 30 * 1024 * 1024
+val STRATEGIES = ExchangeStrategies.builder()
+    .codecs { it.defaultCodecs().maxInMemorySize(SIZE_MB) }
+    .build()
+
+@Component
+class WebClientConfig(
+    @Value("\${client.endpoint.music}") val musicUrl: String,
+    @Value("\${client.endpoint.storage}") val storageUrl: String
+) {
+    @Bean(name = ["musicClient"])
+    @LoadBalanced
+    fun musicClient() = WebClient.builder()
+        .baseUrl(musicUrl)
+        .exchangeStrategies(STRATEGIES)
+
+    @Bean(name = ["storageClient"])
+    fun storageClient() = WebClient.builder()
+        .baseUrl(storageUrl)
+        .exchangeStrategies(STRATEGIES)
+}
