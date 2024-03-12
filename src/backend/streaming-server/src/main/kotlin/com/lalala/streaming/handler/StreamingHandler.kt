079dae93f26e3bdaa011f1e4e44ccc456f6cd52e
kor-Chipmunk <rhj4862@gmail.com>
2024-03-12 11:36:21 +0900

    refactor: WebFlux WebSocket 핸들러 구조로 변경
    - 디스커버리 포함된 `WebClient.Builder` 을 주입받도록 수정
    - `handleTextMessage` 메소드의 큰 변경 없이 `Flux<WebSocketMessage>` 반환 형태로 변경
    - 음원 서버 / 스토리지 서버 통신 로직을 `WebClient` Mono / Flux 구조로 변경
    - 예외 발생 로직을 `Mono.error(Throwable)` 으로 변경
    - `BaseResponse<>` 사용을 위한 `typeReference` 코드 추가
    - 스토리지 서버에서 음원 다운로드 시 `DataBuffer` NIO 버퍼를 사용하도록 변경

--- src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/handler/StreamingHandler.kt
+++ src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/handler/StreamingHandler.kt
@@ -1,195 +1,229 @@
 package com.lalala.streaming.handler
 
+import com.lalala.event.StreamingCompleteEvent
 import com.lalala.exception.BusinessException
 import com.lalala.exception.ErrorCode
 import com.lalala.response.BaseResponse
 import com.lalala.streaming.constant.StreamingConstant
 import com.lalala.streaming.dto.MusicDetailDTO
+import com.lalala.streaming.external.kafka.KafkaProducer
 import com.lalala.streaming.handler.Command.*
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.bramp.ffmpeg.FFmpeg
 import net.bramp.ffmpeg.FFprobe
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.core.io.buffer.DataBuffer
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
-import org.springframework.web.client.RestClient
-import org.springframework.web.client.body
-import org.springframework.web.socket.BinaryMessage
-import org.springframework.web.socket.CloseStatus
-import org.springframework.web.socket.TextMessage
-import org.springframework.web.socket.WebSocketSession
-import org.springframework.web.socket.handler.TextWebSocketHandler
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.socket.WebSocketHandler
+import org.springframework.web.reactive.socket.WebSocketMessage
+import org.springframework.web.reactive.socket.WebSocketSession
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 import java.io.File
 import java.io.IOException
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import java.time.Duration
+import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 import kotlin.math.floor
 
+inline fun <reified T> typeReference() = object : ParameterizedTypeReference<T>() {}
+
+@Component
 class StreamingHandler(
     private val ffMpeg: FFmpeg,
     private val ffProbe: FFprobe,
-    @Qualifier("musicClient")
-    private val musicClient: RestClient,
-    @Qualifier("storageClient")
-    private val storageClient: RestClient,
-) : TextWebSocketHandler() {
-    // TODO: 다음 트랙 변화 이벤트 추가
-
+    @Qualifier("musicClient") private val musicClient: WebClient.Builder,
+    @Qualifier("storageClient") private val storageClient: WebClient.Builder,
+    private val producer: KafkaProducer,
+    @Qualifier("preprocessingExecutor") private val executor: Executor,
+) : WebSocketHandler {
     private val logger = KotlinLogging.logger {}
 
-    override fun afterConnectionEstablished(session: WebSocketSession) {
-        logger.info { "${session.id} - Connected!" }
-        super.afterConnectionEstablished(session)
+    override fun handle(session: WebSocketSession): Mono<Void> {
+        val messages = session.receive()
+            .doOnSubscribe { onConnect(session) }
+            .subscribeOn(Schedulers.parallel())
+            .map { it.payloadAsText }
+            .flatMap { handleTextMessage(session, it) }
+            .onErrorResume {
+                if (it is BusinessException) {
+                    when (it.errorCode) {
+                        ErrorCode.MUSIC_NOT_FOUND -> Flux.just(session.textMessage("failed music"))
+                        ErrorCode.STORAGE_FILE_NOT_FOUND -> Flux.just(session.textMessage("failed storage"))
+                        else -> Flux.just(session.textMessage("unknown error"))
+                    }
+                }
+                Flux.just(session.textMessage("unknown error"))
+            }
+
+        return session.send(messages)
+            .subscribeOn(Schedulers.parallel())
+            .doOnError { it.printStackTrace() }
+            .doFinally { onDisconnect(session) }
     }
 
-    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-        logger.info { "${session.id} - Disconnected! ( ${status.code} - ${status.reason} )" }
+    fun onConnect(session: WebSocketSession) {
+        logger.info { "${session.id} - Connected!" }
+    }
+
+    fun onDisconnect(session: WebSocketSession) {
+        logger.info { "${session.id} - Disconnected!" }
 
         File(StreamingConstant.TEMP_FOLDER)
             .walkTopDown()
             .maxDepth(1)
-            .filter(File::isFile)
-            .filter { it.name.startsWith(session.id) }
-            .forEach(File::delete)
-
-        super.afterConnectionClosed(session, status)
+            .toFlux()
+            .filter { it.isFile && it.name.startsWith(session.id) }
+            .doOnNext(File::delete)
+            .subscribe()
     }
 
-    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
-
-        val payload = message.payload
-
+    fun handleTextMessage(session: WebSocketSession, payload: String): Flux<WebSocketMessage> {
         logger.info { "${session.id} - ${payload}" }
 
         val command = payload.split("/")[0]
         val commandType = Command.entries.find { it.value == command } ?: throw RuntimeException()
 
-        try {
-            when (commandType) {
-                GET -> {
-                    val musicId = payload.split("/")[1]
-                    sendPlayTime(session, musicId)
-                }
-
-                STREAM -> {
-                    val musicId = payload.split("/")[1]
-                    val startTime = payload.split("/")[2]
-                    startStream(session, musicId, startTime)
-                }
-
-                TRACK_CHANGED -> TODO()
+        when (commandType) {
+            GET -> {
+                val musicId = payload.split("/")[1]
+                return sendPlayTime(session, musicId)
             }
-        } catch (err: Exception) {
-            err.printStackTrace()
-            afterConnectionClosed(session, CloseStatus.GOING_AWAY)
+
+            STREAM -> {
+                val musicId = payload.split("/")[1]
+                val startTime = payload.split("/")[2]
+                return startStream(session, musicId, startTime)
+            }
+
+            TRACK_CHANGED -> {
+                val completeMusicId = payload.split("/")[1]
+                producer.execute(StreamingCompleteEvent(completeMusicId.toLong()))
+                return Flux.just(session.textMessage("track changed"))
+            }
         }
     }
 
-    fun getMusic(session: WebSocketSession, musicId: String): BaseResponse<MusicDetailDTO> {
-        return musicClient.get()
+    fun getMusic(session: WebSocketSession, musicId: String): Mono<MusicDetailDTO> {
+        return musicClient.build().get()
             .uri("/v1/api/musics/${musicId}")
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
-            .onStatus(HttpStatusCode::is4xxClientError) { _, _ ->
-                session.sendMessage(TextMessage("failed music"))
-                throw BusinessException(ErrorCode.MUSIC_NOT_FOUND)
+            .onStatus(HttpStatusCode::isError) {
+                Mono.error(BusinessException(ErrorCode.MUSIC_NOT_FOUND))
             }
-            .onStatus(HttpStatusCode::is5xxServerError) { _, _ ->
-                session.sendMessage(TextMessage("failed music"))
-                throw BusinessException(ErrorCode.MUSIC_NOT_FOUND)
+            .bodyToMono(typeReference<BaseResponse<MusicDetailDTO>>())
+            .map {
+                it.data as MusicDetailDTO
             }
-            .body<BaseResponse<MusicDetailDTO>>()!!
     }
 
-    fun sendPlayTime(session: WebSocketSession, musicId: String) {
+    fun sendPlayTime(session: WebSocketSession, musicId: String): Flux<WebSocketMessage> {
         // 음원 서버에서 파일 주소 조회
-        val music = getMusic(session, musicId).data
-        music.playTime.let { session.sendMessage(TextMessage("$it")) }
+        return getMusic(session, musicId)
+            .flatMapMany {
+                Flux.just(session.textMessage("${it.playTime}"))
+            }
     }
 
-    fun startStream(session: WebSocketSession, musicId: String, startTime: String) {
+    fun startStream(session: WebSocketSession, musicId: String, startTime: String): Flux<WebSocketMessage> {
         // 음원 서버에서 파일 주소 조회
-        val music = getMusic(session, musicId).data
-        val fileName = music.file.fileUrl.split('/').last()
+        return getMusic(session, musicId)
+            .map { URI(it.file.fileUrl).path }
+            .flatMap { downloadMusic(session, it) }
+            .then(File("${StreamingConstant.TEMP_FOLDER}/${session.id}.mp3").toMono())
+            .flatMapMany {
+                // 파일 서버에서 파일 다운로드
+                val result = ffProbe.probe(it.path)
+                val format = result.getFormat()
+                val duration = format.duration
 
-        // 파일 서버에서 파일 다운로드
-        downloadMusic(session, fileName)
+                logger.info { "filename: " + format.filename }
+                logger.info { "duration : " + format.duration }
+                logger.info { "bit_rate : " + format.bit_rate }
 
-        val mediaFile = File("${StreamingConstant.TEMP_FOLDER}/${session.id}.mp3")
+                val processBuilder = ProcessBuilder()
+                processBuilder.directory(File(System.getProperty("user.dir")));
+                processBuilder.command(
+                    ffMpeg.path, "-y", "-i", it.path,
+                    "-ss", startTime, "-f", "segment", "-segment_time", "${StreamingConstant.TRIM_DURATION_SEC}",
+                    "${StreamingConstant.TEMP_FOLDER}/${session.id}-%d.flac"
+                )
 
-        val result = ffProbe.probe(mediaFile.path)
-        val format = result.getFormat()
-        val duration = format.duration
-
-        logger.info { "filename: " + format.filename }
-        logger.info { "duration : " + format.duration }
-        logger.info { "bit_rate : " + format.bit_rate }
-
-        val processBuilder = ProcessBuilder()
-        processBuilder.directory(File(System.getProperty("user.dir")));
-        processBuilder.command(
-            ffMpeg.path, "-y", "-i", mediaFile.path,
-            "-ss", startTime, "-f", "segment", "-segment_time", "${StreamingConstant.TRIM_DURATION_SEC}",
-            "${StreamingConstant.TEMP_FOLDER}/${session.id}-%d.flac"
-        )
-
-        val commandLineThread = Thread() {
-            try {
-                val process: Process = processBuilder.start()
-                process.waitFor(10, TimeUnit.SECONDS)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
-            }
-        }
-        commandLineThread.isDaemon = true
-        commandLineThread.start()
-
-        val remainingDuration = duration - startTime.toDouble()
-        val remainingCount = floor(remainingDuration / StreamingConstant.TRIM_DURATION_SEC).toInt()
-
-        for (i in 0..remainingCount) {
-            val nextFile = File(StreamingConstant.TEMP_FOLDER, "${session.id}-${i+1}.flac")
-            while (!nextFile.exists()) {
-                if (!commandLineThread.isAlive && i == remainingCount) {
-                    // 마지막은 다음 파일이 나오는 기준이 아닌, 스레드 종료 기준
-                    break;
+                val commandLineThread = Thread() {
+                    try {
+                        val process: Process = processBuilder.start()
+                        process.waitFor(10, TimeUnit.SECONDS)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
                 }
-                Thread.sleep(10)
+                commandLineThread.isDaemon = true
+                executor.execute(commandLineThread)
+
+                val remainingDuration = duration - startTime.toDouble()
+                val remainingCount = floor(remainingDuration / StreamingConstant.TRIM_DURATION_SEC).toInt()
+
+                return@flatMapMany Flux.interval(Duration.ofMillis(100)).flatMap {
+                    Flux.generate<WebSocketMessage?, Int?>(
+                        { 0 },
+                        { state, sink ->
+                            val nextFile = File(StreamingConstant.TEMP_FOLDER, "${session.id}-${state + 1}.flac")
+                            while (!nextFile.exists()) {
+                                if (!commandLineThread.isAlive && state == remainingCount) {
+                                    // 마지막은 다음 파일이 나오는 기준이 아닌, 스레드 종료 기준
+                                    break;
+                                }
+                            }
+
+                            val splitFile = File(StreamingConstant.TEMP_FOLDER, "${session.id}-${state}.flac")
+                            splitFile.inputStream().buffered().use { stream ->
+                                sink.next(session.binaryMessage { it.wrap(stream.readBytes()) })
+                            }
+                            splitFile.delete()
+
+                            if (state == remainingCount) {
+                                sink.complete()
+                            }
+
+                            return@generate state + 1
+                        }
+                    )
+                }
             }
-
-            val splitFile = File(StreamingConstant.TEMP_FOLDER, "${session.id}-${i}.flac")
-            splitFile.inputStream().buffered().use { stream ->
-                session.sendMessage(BinaryMessage(stream.readBytes(), false))
-            }
-            session.sendMessage(BinaryMessage(ByteArray(0), true))
-
-            splitFile.delete()
-
-            // 다음 전달까지 대기
-            Thread.sleep(100);
-        }
-
-        mediaFile.delete()
     }
 
-    fun downloadMusic(session: WebSocketSession, fileName: String) {
+    fun downloadMusic(session: WebSocketSession, filePath: String): Mono<Void> {
         // 파일 서버에서 파일 다운로드
-        val storageData = storageClient.get()
-            .uri("/${fileName}")
+        val downloadFlux = storageClient.build().get()
+            .uri(filePath)
             .accept(MediaType.APPLICATION_OCTET_STREAM)
             .retrieve()
-            .onStatus(HttpStatusCode::is4xxClientError) { _, _ ->
-                session.sendMessage(TextMessage("failed storage"))
-                throw BusinessException(ErrorCode.STORAGE_FILE_NOT_FOUND)
+            .onStatus(HttpStatusCode::isError) { _ ->
+                Mono.error(BusinessException(ErrorCode.STORAGE_FILE_NOT_FOUND))
             }
-            .onStatus(HttpStatusCode::is5xxServerError) { _, _ ->
-                session.sendMessage(TextMessage("failed storage"))
-                throw BusinessException(ErrorCode.STORAGE_FILE_NOT_FOUND)
-            }
-            .body<ByteArray>()
+            .bodyToFlux(DataBuffer::class.java)
 
-        val downloadFile = File(StreamingConstant.TEMP_FOLDER, "${session.id}.mp3")
-        downloadFile.writeBytes(storageData!!)
+        val outputPath = Paths.get(StreamingConstant.TEMP_FOLDER, "${session.id}.mp3")
+        return Mono.fromRunnable<Boolean> {
+            Files.deleteIfExists(outputPath)
+        }
+            .then(
+                DataBufferUtils.write(downloadFlux, outputPath, StandardOpenOption.CREATE)
+            )
     }
 }
