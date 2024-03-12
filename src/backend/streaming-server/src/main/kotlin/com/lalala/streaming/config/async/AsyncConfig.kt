100faf40f4c8564e11cd5a089576ac457a709f09
kor-Chipmunk <rhj4862@gmail.com>
2024-03-12 11:29:57 +0900

    feat: `KafkaProducer` 발행 Config 추가

---
+++ src/backend/streaming-server/src/main/kotlin/com/lalala/streaming/config/async/AsyncConfig.kt
@@ -0,0 +1,28 @@
+package com.lalala.streaming.config.async
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy
+
+
+@Configuration
+@EnableAsync
+internal class AsyncConfig {
+    @Bean(name = ["asyncProducerExecutor"])
+    fun asyncProducerExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 10
+        executor.maxPoolSize = 20
+        executor.queueCapacity = 40
+        executor.setThreadNamePrefix("Producer-Thread")
+        executor.keepAliveSeconds = 30
+        executor.setAllowCoreThreadTimeOut(false)
+        executor.setPrestartAllCoreThreads(true)
+        executor.setRejectedExecutionHandler(CallerRunsPolicy())
+        executor.initialize()
+        return executor
+    }
+}
